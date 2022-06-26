cc89e5a247dae471db7068b2a90238f4a04efd86
Nsrri <nasrin.jafari@powercoders.orgr>
2022-06-26 18:47:50 +0200

    Add .gitignore file

---
+++ Dictionary-De/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dictionary-De
+//
+//  Created by Nasrin Jafari on 26.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dictionary-De/Dictionary_DeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dictionary_DeApp.swift
+//  Dictionary-De
+//
+//  Created by Nasrin Jafari on 26.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Dictionary_DeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
