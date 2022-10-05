4353898ba0327b39df9003c8b81631d6af005ade
Nasrin Jafari <nasrin.jafari@powwrcoders.org>
2022-10-05 15:32:54 +0200

    added tabView

---
+++ Dictionary-De/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  Worterbuch
+//
+//  Created by NasrinJafari(Personal) on 05.10.22.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Here you are able to search and see some optinal words")
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
---
+++ Dictionary-De/MainView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Dictionary-De
+//
+//  Created by Nasrin Jafari on 26.06.22.
+//
+
+import SwiftUI
+import UIKit
+
+struct MainView: View {
+    var Wort = Bundle.main.decode([Module].self, from: "words.json")
+    var body: some View {
+
+        GeometryReader{ geometry in
+            VStack{
+                        ForEach(Wort) { x in
+                            Text(x.article)
+                        }
+            }
+            TabView{
+                Home()
+                    .tabItem {
+                        Label("Home", systemImage: "Home")
+                    }
+                Favorites()
+                    .tabItem {
+                        Label("Favorites", systemImage: "star")
+                    }
+                Add()
+                    .tabItem {
+                        Label("Add words", systemImage: "plus")
+                    }
+            }
+  
+        }
+       
+                    
+        }
+
+    }
+
