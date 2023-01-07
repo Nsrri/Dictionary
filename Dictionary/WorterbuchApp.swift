5867ae2bb43c598788bb4e49bd24e7fc4b94ddfb
Nasrin Jafari <nasrin.jafari@powwrcoders.org>
2023-01-07 22:14:19 +0100

    fix the stateObject warning

--- Dictionary/WorterbuchApp.swift
+++ Dictionary/WorterbuchApp.swift
@@ -10,13 +10,13 @@ import SwiftUI
 @main
 struct Worterbuch: App {
     @StateObject var dataController = DataController()
-    init(){
-        dataController.preloadData()
-    }
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, dataController.getContext())
+                .onAppear(){
+                    dataController.preloadData()
+                }
         }
     }
 }
