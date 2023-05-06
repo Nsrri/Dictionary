768a33fa7166f2bd73480c9e811859db4734ec3c
Nsrri <nasrin.jafari@powercoders.orgr>
2023-05-06 20:21:24 +0200

    hotFix

---
+++ Dictionary-De/Add.swift
@@ -0,0 +1,20 @@
+//
+//  Add.swift
+//  Worterbuch
+//
+//  Created by NasrinJafari(Personal) on 05.10.22.
+//
+
+import SwiftUI
+
+struct Add: View {
+    var body: some View {
+        Text("From here you can add new words to our app")
+    }
+}
+
+struct Add_Previews: PreviewProvider {
+    static var previews: some View {
+        Add()
+    }
+}
---
+++ Dictionary-De/MainView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Dictionary-De
+//
+//  Created by Nasrin Jafari on 26.06.22.
+//
+
+import SwiftUI
+import UIKit
+
+struct MainView: View {
+    var Wort = Bundle.main.decode([WordsInformations].self, from: "words.json")
+    @State var searchText: String = ""
+    var body: some View {
+
+        GeometryReader{ geometry in
+            VStack{
+                        ForEach(Wort) { x in
+                            Text(x.article)
+                        }
+            }
+            TabView{
+                Home()
+                    .tabItem {
+                        Label("Home", systemImage: "house")
+                    }
+        
+                SearchView()
+                    .tabItem{
+                        Label("Search", systemImage: "magnifyingglass")
+                    }
+                Favorites()
+                    .tabItem {
+                        Label("Favorites", systemImage: "star")
+                    }
+                Add()
+                    .tabItem {
+                        Label("Add words", systemImage: "plus")
+                    }
+            }
+  
+        }
+       
+                    
+        }
+
+    }
+
